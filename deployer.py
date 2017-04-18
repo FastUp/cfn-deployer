e3c30a0aa442bc9a2e35c5da8c3354f30e23171e
Sachin <sdole@genvega.com>
2017-04-17 23:04:35 -0500

    v0.1

---
+++ deployer.py
@@ -0,0 +1,406 @@
+from __future__ import print_function
+import sys
+import argparse
+import hashlib
+import json
+import os
+import zipfile
+import platform
+import os.path
+import imp
+import yaml
+
+import boto3
+from botocore.exceptions import ClientError
+
+tmp_folder = 'C:\\temp\\' if platform.system() == 'Windows' else "/tmp/"
+
+boto3_session = None
+
+
+def _zip_dir(path, a_zip_file):
+    for root, dirs, files in os.walk(path):
+        for a_file in files:
+            a_zip_file.write(os.path.join(root, a_file))
+    a_zip_file.close()
+
+
+def release_lambda(config, each_lambda_config):
+    lambda_folder = each_lambda_config["folder"]
+    release_bucket = config["release_bucket"]
+    version = config["version"]
+    project_name = config["project_name"]
+    os.chdir('lambda' + "/" + lambda_folder)
+    code_zip_file_name = lambda_folder + '.zip'
+    lambda_code_zip = zipfile.ZipFile(tmp_folder + code_zip_file_name, 'w')
+    env = config["env"]
+    _zip_dir(".", lambda_code_zip)
+    s3_client = boto3_session.resource('s3')
+    lambda_code_zip_key = project_name + "/" + version + "/" + env + '/lambda/' + code_zip_file_name
+    lambda_code_zip_hash_key = project_name + "/" + version + "/" + env + '/lambda/' + code_zip_file_name + ".md5"
+    lambda_code_hash_s3_obj = s3_client.Object(release_bucket, lambda_code_zip_hash_key)
+    print(lambda_code_zip_hash_key)
+    BLOCKSIZE = 65536
+    hasher = hashlib.md5()
+    with open(tmp_folder + code_zip_file_name, 'rb') as afile:
+        buf = afile.read(BLOCKSIZE)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = afile.read(BLOCKSIZE)
+    deployed_hash = None
+    new_hash = hasher.hexdigest()
+    print(new_hash)
+    try:
+        deployed_hash = lambda_code_hash_s3_obj.get()["Body"].read().decode("utf-8")
+        print(deployed_hash)
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "NoSuchKey":
+            print(
+                "MD5 hashcode of lambda code in folder " + lambda_folder + " was never uploaded or has been deleted. " +
+                "Will upload new MD5 hashcode "
+                + new_hash
+            )
+        else:
+            raise e
+    if deployed_hash is None or deployed_hash != new_hash:
+        print(
+            "MD5 hashcode of deployed lambda code in folder " + lambda_folder + "(" + str(deployed_hash) +
+            ") is not same as MD5 hashcode of new code (" + new_hash +
+            "). Will upload new code."
+        )
+        print("\ts3://" + release_bucket + "/" + lambda_code_zip_hash_key)
+        lambda_code_hash_s3_obj.put(Body=bytes(new_hash))
+        lambda_code_s3_obj = s3_client.Object(release_bucket, lambda_code_zip_key)
+        print("\ts3://" + release_bucket + "/" + lambda_code_zip_key)
+        lambda_code_s3_obj.upload_file(tmp_folder + code_zip_file_name)
+
+    else:
+        print(
+            "MD5 hashcode of deployed lambda code in folder " + lambda_folder + " (" + deployed_hash +
+            ") is same as MD5 hashcode of new code " + new_hash +
+            ". Will not upload new code."
+        )
+
+    os.chdir('../..')
+    code_zip_file_name = lambda_folder + '.zip'
+    lambda_code_zip_key = project_name + "/" + version + "/" + env + '/lambda/' + code_zip_file_name
+    with open("cloudformation/" + config["template"], mode="r+") as template_file:
+        s3_client = boto3_session.client('s3')
+        object_info = s3_client.head_object(
+            Bucket=release_bucket, Key=lambda_code_zip_key
+        )
+        template_contents = json.load(template_file)
+        template_contents["Resources"][each_lambda_config["logical_resource_name"]]["Properties"]["Code"] = {
+            "S3Bucket": release_bucket,
+            "S3Key": lambda_code_zip_key,
+            "S3ObjectVersion": object_info["VersionId"]
+        }
+        template_file.seek(0)
+        json.dump(template_contents, template_file, indent=2)
+        template_file.truncate()
+
+
+def do_release(config):
+    if "lambda_code" in config:
+        print("Uploading lambda code: ")
+        for each_lambda_folder in config["lambda_code"]:
+            release_lambda(
+                config,
+                each_lambda_folder
+
+            )
+
+
+def do_change(config):
+    cfn_client = boto3_session.client('cloudformation')
+    change_set_num = 0
+    if "change_set_number" in config:
+        change_set_num = config["change_set_number"]
+    change_set_num += 1
+    config["change_set_number"] = change_set_num
+    with open(args.config, "w") as config_file:
+        yaml.dump(config, config_file, default_flow_style=False)
+    stack_arguments = make_stack_arguments(config)
+    stack_arguments["ChangeSetName"] = "ChangeSet" + "-" + str(config["change_set_number"])
+    print_arguments(stack_arguments)
+    cfn_client.create_change_set(**stack_arguments)
+
+
+def make_stack_arguments(config):
+    stack_name = create_stack_name(config)
+    data = get_template_as_string(config)
+    stack_arguments = {
+        "StackName": stack_name,
+        "TemplateBody": data,
+    }
+    if "template_parameters" in config:
+        stack_arguments["Parameters"] = json.load(
+            open(
+                "cloudformation/" + config["template_parameters"]
+            )
+        )
+    if args.iam_capabilities is not None:
+        stack_arguments["Capabilities"] = [args.iam_capabilities]
+    if "create_arguments" in config:
+        config["create_arguments"].pop("StackName", None)
+        config["create_arguments"].pop("TemplateBody", None)
+        config["create_arguments"].pop("TemplateURL", None)
+        config["create_arguments"].pop("Parameters", None)
+        config["create_arguments"].pop("Capabilities", None)
+        stack_arguments.update(config["create_arguments"])
+    return stack_arguments
+
+
+def create_stack_name(config):
+    env_suffix = "-" + get_env(config)
+    ver_suffix = "-" + get_ver(config).replace(".", "-")
+    stack_name = config["project_name"] + ver_suffix + env_suffix
+    return stack_name
+
+
+def print_arguments(create_stack_arguments):
+    import copy
+    deepcopy = copy.deepcopy(create_stack_arguments)
+    deepcopy["TemplateBody"] = "Removed"
+    print(deepcopy)
+
+
+def get_template_as_string(config):
+    template_stream = open("cloudformation/" + config["template"])
+    data = ""
+    lines = template_stream.readlines()
+    for line in lines:
+        data += line
+    return data
+
+
+def do_create(config):
+    if "build_helper" in config:
+        helper = imp.load_source("helper", config["build_helper"])
+        dynamic_params = helper.create_dynamic_template_parameters(config)
+
+    cfn_client = boto3_session.client('cloudformation')
+    stack_arguments = make_stack_arguments(config)
+    print_arguments(stack_arguments)
+    create_stack_response = cfn_client.create_stack(**stack_arguments)
+    print("Create Stack Response : " + json.dumps(create_stack_response))
+
+
+def do_cost(config):
+    cfn_client = boto3_session.client('cloudformation')
+    stack_arguments = {
+        "TemplateBody": get_template_as_string(config),
+    }
+    if "template_parameters" in config:
+        stack_arguments["Parameters"] = json.load(
+            open(
+                "cloudformation/" + config["template_parameters"]
+            )
+        )
+
+    cost = cfn_client.estimate_template_cost(**stack_arguments)
+    print(cost)
+
+
+def do_delete(config):
+    stack_name = create_stack_name(config)
+    print(
+        "This will delete stack " + stack_name + ". "
+                                                 "Please confirm by entering the statement without the quotes: "
+                                                 "\"Yes, I want to delete this stack.\""
+    )
+    import sys
+    line = sys.stdin.readline().strip()
+    print(line)
+    if not line == "Yes, I want to delete this stack.":
+        print("OK, not deleting.")
+        sys.exit(1)
+    print("OK, deleting stack " + stack_name)
+    cfn_client = boto3_session.client('cloudformation')
+    stack_arguments = {"StackName": stack_name}
+    if "resources_to_retain" in config:
+        stack_arguments.update({"RetainResources": config["resources_to_retain"]})
+    cfn_client.delete_stack(**stack_arguments)
+
+
+args = None
+
+
+def do_exec_change(config):
+    stack_name = create_stack_name(config)
+    cfn_client = boto3_session.client('cloudformation')
+    method_args = {"ChangeSetName": args.change_set_name, "StackName": stack_name}
+    cfn_client.execute_change_set(**method_args)
+
+
+def do_init():
+    if os.path.isfile("project.yaml"):
+        print("project.yaml already exists. Cannot create. Exiting.")
+        sys.exit(0)
+    if not os.path.exists("cloudformation"):
+        print("directory named cloudformation does not exist. Creating.")
+        os.mkdir("cloudformation")
+    if os.path.isfile("cloudformation/template.yaml") or os.path.isfile("cloudformation/template.config.json"):
+        print("Either template.yaml or template.config.json already exists. Cannot create. Exiting.")
+        sys.exit(0)
+    with(open("project.yaml", "w+")) as new_project_file:
+        yaml.dump(
+            {
+                "env": "dev",
+                "project_name": "ChangeMe",
+                "region": "us-east-1",
+                "template": "template.yaml",
+                "template_parameters": "template.config.json",
+                "version": "1.0.0"
+            },
+            new_project_file,
+            default_flow_style=False
+        )
+    with(open("cloudformation/template.yaml", "w+")) as new_template:
+        yaml.dump(
+            {
+                "AWSTemplateFormatVersion": "2010-09-09",
+                "Description": "ChangeMe",
+                "Resources": {
+                    "AResource": {
+                        "Type": "AWS::EC2::Instance",
+                        "Properties":
+                            {
+                                "ImageId": "\!Ref AParameter"
+                            }
+                    }
+
+                }
+            },
+            new_template,
+            default_flow_style=False
+        )
+    with(open("cloudformation/template.config.json","w+")) as new_template_config:
+        json.dump(
+            [
+                {
+                    "ParameterKey": "AParameter",
+                    "ParameterValue": "ami-22ce4934"
+                }
+            ],
+            new_template_config,
+            indent=4
+        )
+    pass
+
+
+def run():
+    parse_args()
+    current_module = sys.modules[__name__]
+    if args.target == "init":
+        do_init()
+    else:
+        try:
+            print(args)
+            build_config = yaml.load(open(args.config))
+            credential_profile = build_config[
+                "credential_profile"] if "credential_profile" in build_config else "default"
+            if "region" in build_config:
+                session_config = {
+                    "profile_name": credential_profile,
+                    "region_name": build_config["region"]
+                }
+            else:
+                session_config = {
+                    "profile_name": credential_profile
+                }
+
+            current_module.boto3_session = boto3.session.Session(**session_config)
+        except IOError as e:
+            print(
+                "Cannot continue. Could not open file " + args.config + ". Please ensure it exists and is readable.")
+            raise e
+
+        if args.target == "package":
+            do_release(build_config)
+        elif args.target == "create":
+            do_create(build_config)
+        elif args.target == "change":
+            do_change(build_config)
+        elif args.target == "cost":
+            do_cost(build_config)
+        elif args.target == "delete":
+            do_delete(build_config)
+        elif args.target == "exec-change":
+            do_exec_change(build_config)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        description='''
+        AWS Deployer
+        ============
+        Use this to release and deploy cloudformation templates and lambda functions.
+        ''')
+    parser.add_argument(
+        "target",
+        choices=["package", "create", "change", "exec-change", "cost", "delete", "init"],
+        help='''
+        package - zips up lambda code and uploads to S3 under a folder structure reflecting the project
+        name, version and environment as configured in config yaml.
+
+        create - creates the cloudformation stack in AWS. The stack name includes the project name, environment and
+        version. This will also create all resources in the stack. This does not wait for the stack creation to be
+        completed, successful or failed.
+
+        change - creates a change set for an existing stack with the same project name, environment and version.
+
+        cost - estimates cost of the template.
+
+        delete - delete the stack. This is permanent and irreversible.
+
+        init - create a single empty cloudformation template and a configuration file
+        
+        '''
+    )
+    parser.add_argument(
+        "--config",
+        default="project.yaml",
+        help='''
+        Set this to a file path that contains the build configuration. Defaults to project.yaml in the current dir.
+        '''
+    )
+    parser.add_argument(
+        "--iam-capabilities",
+        help='''
+        Set to iam to acknowledge that the template contains IAM resources. See Cloudformation documentation for valid
+        values
+        '''
+    )
+    parser.add_argument(
+        "--change-set-name",
+        help='''
+        If executing a change set, this must be set.
+        '''
+    )
+    current_module = sys.modules[__name__]
+    current_module.args = parser.parse_args()
+    print(args)
+    if args.target == "exec-change" and not hasattr(args, "change_set_name"):
+        parser.error("target exec_change must be accompanied by option --change-set-name")
+    return args
+
+
+def get_config_property(config, key, default):
+    if key in config:
+        return config[key]
+    else:
+        return default
+
+
+def get_env(config):
+    return get_config_property(config, "env", "")
+
+
+def get_ver(config):
+    return get_config_property(config, "version", "")
+
+
+if __name__ == "__main__":
+    run()
---
+++ setup.py
@@ -0,0 +1,88 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='cfndeployer',
+
+
+    version='0.0.1',
+
+    description='A tool to deploy cloudformation stacks',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/pypa/sampleproject',
+
+    # Author details
+    author='Sachin Dole',
+    author_email='sdole@fastup.io',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7'
+    ],
+
+    # What does your project relate to?
+    keywords='AWS cloudformation stack deployment',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['boto3','pyyaml'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'deployer=deployer:run',
+        ],
+    },
+)
